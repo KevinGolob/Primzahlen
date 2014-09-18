324a11fa283338a85493cff6520465d777f4ff32
KevinGolob <kevin.golob@hotmail.de>
2014-09-18 07:18:28 +0200

    Primzahlen bis 50000

--- src/PrimzahlBerechnen.java
+++ src/PrimzahlBerechnen.java
@@ -10,7 +10,7 @@ public class PrimzahlBerechnen {
 	
 	
 	public static void berechnen(){
-		for (int i=2; i<=10000; i++){
+		for (int i=2; i<=50000; i++){
 			boolean prime = true;
 			for (int j=2; j<i; j++){
 				if(i%j == 0){
