64ae7433c4b0219e2cf10e1c14fa459cc217284d
KevinGolob <kevin.golob@hotmail.de>
2014-09-18 07:13:49 +0200

    Commit Github

---
+++ src/PrimzahlBerechnen.java
@@ -0,0 +1,29 @@
+
+public class PrimzahlBerechnen {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		berechnen();
+		
+	}
+	
+	
+	public static void berechnen(){
+		for (int i=2; i<=10000; i++){
+			boolean prime = true;
+			for (int j=2; j<i; j++){
+				if(i%j == 0){
+					prime= false;
+					break;
+				}
+			}
+			if (prime){
+				System.out.println("Primzahl: "+i);
+			}
+			
+		}
+	}
+	
+}
+
